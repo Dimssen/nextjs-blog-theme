644a0c43fe798c687986621fc358d813501ef3f3
dim <72107949+Dimssen@users.noreply.github.com>
2024-09-16 22:41:46 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,11 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <img
+        src="https://cdn.discordapp.com/attachments/938769562206367766/1216843348300529664/Logo2.png?ex=66e9e4ae&is=66e8932e&hm=eabea6f78cc93bdbc97dd241c408e7f4cfebf0fc510e2d47a9fa22ca28b3c271&"
+        alt="Dim's Capes"
+        className="block w-12 h-12 mx-auto mb-4 rounded-full"
+      />
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">
           {name}
