b668c7c16677e33cf66ea719a9abd3fa75711518
dim <72107949+Dimssen@users.noreply.github.com>
2024-09-16 22:51:20 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header({ name }) {
       <img
         src="https://cdn.discordapp.com/attachments/938769562206367766/1216843348300529664/Logo2.png?ex=66e9e4ae&is=66e8932e&hm=eabea6f78cc93bdbc97dd241c408e7f4cfebf0fc510e2d47a9fa22ca28b3c271&"
         alt="Dim's Capes"
-        className="block w-18 h-18 mx-auto mb-4 rounded-full"
+        className="block w-15 h-15 mx-auto mb-4 rounded-full"
       />
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">
